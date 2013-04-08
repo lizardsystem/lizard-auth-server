edca0ed4d7a03c0cd36a0ff132d6a9b89c374203
byrman <carsten.byrman@nelen-schuurmans.nl>
2013-04-08 11:29:25 +0200

    Add is_active to the list of keys to be dumped as json

--- lizard_auth_server/utils.py
+++ lizard_auth_server/utils.py
@@ -7,7 +7,7 @@ import string
 
 
 # Note: the code in this module must be identical in both lizard-auth-server
-# and lizard-auth-client! 
+# and lizard-auth-client!
 
 random = SystemRandom()
 KEY_CHARACTERS = string.letters + string.digits
@@ -20,12 +20,15 @@ SIMPLE_KEYS = [
     'first_name',
     'last_name',
     'email',
+    'is_active',
     'is_staff',
     'is_superuser',
 ]
 
+
 def default_gen_secret_key(length=40):
     return ''.join([random.choice(KEY_CHARACTERS) for _ in range(length)])
+
 
 def gen_secret_key(length=40):
     generator = getattr(settings, 'SSO_KEYGENERATOR', default_gen_secret_key)

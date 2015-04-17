987f5e9584fc81de49c094934c10fe5ad20c999b
Reinout van Rees <reinout@vanrees.org>
2015-04-17 12:40:09 +0000

    Removed tests.py that is overshadowed by tests/__init__.py

--- lizard_auth_server/tests.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.test import TestCase
-
-
-class ExampleTest(TestCase):
-    def test_something(self):
-        self.assertEquals(1, 1)

bef2dd897d265f080045a8403a5716324090875d
Reinout van Rees <reinout@vanrees.org>
2020-11-09 14:47:58 +0100

    Upgraded to TEMPLATES and MIDDLEWARE settings

--- lizard_auth_server/testsettings.py
+++ lizard_auth_server/testsettings.py
@@ -139,29 +139,40 @@ CACHES = {
     }
 }
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.contrib.messages.context_processors.messages",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.request",
-)
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                # Default list above.
+                "django.core.context_processors.i18n",
+                "django.core.context_processors.media",
+                "django.core.context_processors.static",
+            ]
+        },
+    },
+]
 
-MIDDLEWARE_CLASSES = (
-    # Gzip needs to be at the top.
-    "django.middleware.gzip.GZipMiddleware",
-    "django.middleware.common.CommonMiddleware",
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Default list above.
     "django.middleware.locale.LocaleMiddleware",
     "tls.TLSRequestMiddleware",
-)
+]
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     "lizard_auth_server",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -172,7 +183,7 @@ INSTALLED_APPS = (
     "django.contrib.staticfiles",
     "oidc_provider",
     "django_extensions",
-)
+]
 
 DEFAULT_FROM_EMAIL = "noreply@nelen-schuurmans.nl"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"

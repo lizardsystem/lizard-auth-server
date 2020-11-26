4344e6ac8e248a8d8c5f6c34ae0ac82e20bd9944
Reinout van Rees <reinout@vanrees.org>
2020-11-26 15:53:13 +0100

    Fixed bug. form wasn't passed.
    According to
    http://ccbv.co.uk/projects/Django/1.11/django.views.generic.edit/FormView/, a
    quick form_class=None fixes it.
    
    Indeed, in 1.6 the `=None` part wasn't present yet.

--- lizard_auth_server/views.py
+++ lizard_auth_server/views.py
@@ -174,7 +174,9 @@ class EditProfileView(FormView):
             "last_name": self.profile.last_name,
         }
 
-    def get_form(self, form_class):
+    def get_form(self, form_class=None):
+        if form_class is None:
+            form_class = self.get_form_class()
         return form_class(user=self.request.user, **self.get_form_kwargs())
 
     def form_valid(self, form):
